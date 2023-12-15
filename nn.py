0a1353370fe67358c42c281fa52f66f7486daed6
shmartin <abadjuanmartin@gmail.com>
2023-12-15 21:15:12 +0800

    cleanup

--- nn.py
+++ nn.py
@@ -1,4 +1,7 @@
+import time
+import matplotlib.pyplot  as plt
 import torch
+from PIL import Image
 from torch import nn, save, load
 from torch.optim import Adam
 from torch.utils.data import DataLoader
@@ -6,7 +9,7 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 
 train = datasets.MNIST(root = 'data', download = True, train = True, transform = ToTensor())
-dataset = DataLoader(train, 32)
+dataset = DataLoader(train, 6144)
 
 class dimir_eyes(nn.Module):
     dillon = 1
@@ -26,39 +29,101 @@ class dimir_eyes(nn.Module):
     def forward(self, x):
         return self.model(x)
 
-def main():
-    clf = dimir_eyes().to('cuda')
-    opt = Adam(clf.parameters(), lr = 1e-3)
-    loss_fn = nn.CrossEntropyLoss()
-
-    for epoch in range(100):
-        for batch in dataset: 
-            X, y = batch
-            X, y = X.to('cuda'), y.to('cuda')
-            yhat = clf(X)
-            loss = loss_fn(yhat, y)
-
-            opt.zero_grad()
-            loss.backward()
-            opt.step()
-        
-        print(f'Epoch {epoch} loss is {loss.item()}')
+class ml_learning:
+    def __init__(self):
+        # !!!Note: only train if you are prepared to restart your pc afterwards.
+        # CUDA/GPU cleanup is not yet implemented.
+        # Gaming after training will result in lower framerates as
+        # there are residual CUDA, model and driver cache in the VRAM.
+        self.train = True # set if learning or recognizing
+        # will save a model and will be referenced even in discrete runtimes
     
-    with open('model_state.pt', 'wb') as f:
-        save(clf.state_dict(), f)
+        self.device = 'cuda' # set to cpu or cuda depending on hardware
+        # I am using ROCm HIP that's why I can use cuda even on AMD hardware
+
+        self.clf = dimir_eyes().to(self.device)
+
+        if self.train:
+            iteration = int(input('Enter number of iteration to train: '))
+            self.learning(iteration)
+            self.graph()
+        else:
+            self.recognizing()
+
+    def recognizing(self):
+        print('Starting main function')
+        opt = Adam(self.clf.parameters(), lr = 1e-3)
+        loss_fn = nn.CrossEntropyLoss()
+    
+        with open('model_state.pt', 'rb') as f:
+            self.clf.load_state_dict(load(f))
+    
+        img = Image.open('image.jpg')
+        img_tensor = ToTensor()(img).unsqueeze(0).to(self.device)
+        
+        print(torch.argmax(self.clf(img_tensor)))
+    
+    
+    def learning(self, iteration): # training will reset the model evertime it is run, for now
+        print('Starting Training')
+
+        dur = []
+        total_time = []
+
+        self.graph_iteration = []
+        self.graph_loss = []
+        self.graph_time = []
+
+        opt = Adam(self.clf.parameters(), lr = 1e-3)
+        loss_fn = nn.CrossEntropyLoss()
+
+        for epoch in range(1, iteration + 1):
+            t0 = time.time()
+            for batch in dataset: 
+                X, y = batch
+                X, y = X.to(self.device), y.to(self.device)
+                yhat = self.clf(X)
+                loss = loss_fn(yhat, y)
+    
+                opt.zero_grad()
+                loss.backward()
+                opt.step()
+            
+            t1 = time.time() - t0
+            print(f'Epoch: {epoch}; Loss: {loss.item()}; Time(s): {t1}')
+            total_time.append(t1)
+
+            self.graph_iteration.append(epoch)
+            self.graph_loss.append(loss.item())
+            self.graph_time.append(time)
+        
+        with open('model_state.pt', 'wb') as f:
+            save(self.clf.state_dict(), f)
+
+        summation = 0
+        for i in total_time:
+            summation = summation + i
+
+        print(f'Total time is {summation / 60} minutes')
+
+    def graph(self):
+        x = self.graph_iteration
+        y = self.graph_loss
+        plt.plot(x,y)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.title('Loss per epoch')
+        plt.savefig('traning_graph.png')
 
 
-
-    def would(self):
-        pass
-
-def test():
+def test(): # device test if working
+    print('Starting test function')
     #train()
-    amdgpu = False
+    amdgpu = True
     print(torch.cuda.get_device_name(0))
 
     print()
-    if torch.cuda.is_available():
+    if torch.cuda.is_available() and amdgpu:
         device = 'cuda'
         print(f'\nCUDA is available. Enabling CUDA.')
     else:
@@ -75,5 +140,6 @@ def test():
     print(C)
     #print(C)
 
+
 if __name__ == "__main__":
-    main()
+    ml_learning()
